e062a04cf870b8f45e8ca84397280df90ee20cd2
akirayoshihara <ahrasia@gmail.com>
2016-04-02 03:13:13 +0000

    try BookmarkApp

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-    #追加
-  resources :posts do
-    get "bookmarks/toggle"
-  end
-
+  root to: 'posts#index'
+  devise_for :users
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -58,4 +56,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :posts do
+    get "bookmarks/toggle"
+  end
 end
